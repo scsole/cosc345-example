a95648b95af69baa6897d7a1dd3c695a911ff208
Sebastian Sole <scsole@protonmail.com>
2021-05-28 13:08:19 +1200

    Add kdoc

--- app/src/main/java/xyz/scsole/myawesomeapp/ui/login/LoggedInUserView.kt
+++ app/src/main/java/xyz/scsole/myawesomeapp/ui/login/LoggedInUserView.kt
@@ -2,6 +2,8 @@ package xyz.scsole.myawesomeapp.ui.login
 
 /**
  * User details post authentication that is exposed to the UI
+ *
+ * @property displayName The user's display name
  */
 data class LoggedInUserView(
         val displayName: String
